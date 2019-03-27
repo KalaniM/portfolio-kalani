de2d2a63b7d6ed687358d04bb2144895c9e892af
edwinvautier <edwin.vautier@gmail.com>
2019-03-27 09:31:15 +0100

    page system base

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,15 @@
  * * This class is used to handle which screen to display on the portfolio
  * @param name The name of the screen you want to create
  * @param link The link of the image you want to display on the screen
+ * @param description The html for the description part
  */
 class Screen {
-    constructor(name, link) {
-        this.name = name
-        this.link = link
-    }
+  constructor(name, link, description, date) {
+    this.description = description;
+    this.name = name;
+    this.link = link;
+    this.date = date;
+  }
 }
 
 /**
@@ -15,49 +18,93 @@ class Screen {
  * @param screens A table which contains the screens to display
  */
 class Display {
-    constructor(screens) {
-        this.screens = screens
-        this.currentScreen = screens[0]
+  constructor(screens) {
+    this.screens = screens;
+    this.currentScreen = screens[0];
 
-        this.display = (screenName) => {
-            for (let i = 0; i < this.screens.length; i++) {
-                const screen = this.screens[i];
-                if(screen.name === screenName) {
-                    this.currentScreen = screens[i]
-                    this.refreshScreen()
-                }
+    this.display = screenName => {
+      for (let i = 0; i < this.screens.length; i++) {
+        const screen = this.screens[i];
+        if (screen.name === screenName) {
+          this.currentScreen = screens[i];
+          this.refreshScreen();
+        }
+      }
+    };
+    this.refreshScreen = () => {
+      let rightImage = document.querySelector(".right .background img");
+      rightImage.setAttribute("src", this.currentScreen.link);
+    };
+
+    this.getPage = screenName => {
+      let lis = document.querySelectorAll(".projects__project");
+      let middleToTop = document.querySelector(".middle").offsetTop;
+
+      for (let i = 0; i < this.screens.length; i++) {
+        const screen = this.screens[i];
+        if (screen.name === screenName) {
+          lis.forEach(li => {
+            if (li.classList.contains("active")) {
+              li.classList.add("highlighted");
+              let liToTop = li.offsetTop;
+              let toScroll = liToTop - middleToTop - 100;
+              li.style.transform = "translateY(-" + toScroll + "px)";
+            } else {
+              li.style.transform = "translateX(-1000px)";
             }
+          });
         }
-        this.refreshScreen = () => {
-            let rightImage = document.querySelector('.right .background img')
-            rightImage.setAttribute('src', this.currentScreen.link)
-        }
-    }
+      }
+    };
+  }
 }
 
 // Creating the screens
 
-let nespresso = new Screen('nespresso', './assets/images/nespresso.jpg')
-let dassault = new Screen('dassault', './assets/images/dassault.jpg')
-let powerpoint = new Screen('templating powerpoint', './assets/images/powerpoint.jpg')
-let socomptoir = new Screen('so comptoir', './assets/images/socomptoir.jpg')
-let tothetop = new Screen('tothetop', './assets/images/tothetop.png')
-let galbobain = new Screen('galbobain', './assets/images/galbobain.jpg')
+let nespresso = new Screen(
+  "nespresso",
+  "./assets/images/nespresso.jpg",
+  "lorem"
+);
+let dassault = new Screen("dassault", "./assets/images/dassault.jpg", "lorem");
+let powerpoint = new Screen(
+  "templating powerpoint",
+  "./assets/images/powerpoint.jpg",
+  "lorem"
+);
+let socomptoir = new Screen(
+  "so comptoir",
+  "./assets/images/socomptoir.jpg",
+  "lorem"
+);
+let tothetop = new Screen("tothetop", "./assets/images/tothetop.png", "lorem");
+let galbobain = new Screen(
+  "galbobain",
+  "./assets/images/galbobain.jpg",
+  "lorem"
+);
 
 // The display is the portfolio, and we just have a set of screens to display
-let portfolio = new Display([nespresso, dassault, powerpoint, socomptoir, tothetop, galbobain])
+let portfolio = new Display([
+  nespresso,
+  dassault,
+  powerpoint,
+  socomptoir,
+  tothetop,
+  galbobain
+]);
 
 // Selects the nav and handles screen changes
-let nav = document.querySelectorAll('.projects__project')
+let nav = document.querySelectorAll(".projects__project");
 
 nav.forEach(link => {
-    link.addEventListener('mouseenter', () => {
-        portfolio.display(link.innerHTML.toLowerCase())
-        document.querySelector('li.active').classList.remove('active')
-        link.classList.add('active')
-        document.querySelector('.background').scrollTop = 10
-    })
-})
-
-
-
+  link.addEventListener("mouseenter", () => {
+    portfolio.display(link.innerHTML.toLowerCase());
+    document.querySelector("li.active").classList.remove("active");
+    link.classList.add("active");
+    document.querySelector(".background").scrollTop = 10;
+  });
+  link.addEventListener("click", () => {
+    portfolio.getPage(link.innerHTML.toLowerCase());
+  });
+});
