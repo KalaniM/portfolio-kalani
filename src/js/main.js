289b99cebb6e192e54ce4df4ef4dcf3a801ff19b
edwinvautier <edwin.vautier@gmail.com>
2019-03-29 16:31:32 +0100

    mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,26 @@
 
 // Mobile handling
-function detectmob() {
-  if(window.innerWidth <= 600) {
-    return true;
-  } else {
-    return false;
+var isMobile = {
+  Android: function() {
+      return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+      return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+      return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+      return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+  },
+  any: function() {
+      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
   }
-}
-if(detectmob()) {
+};
+if(isMobile.any()) {
   let body = document.querySelector('body')
   body.innerHTML = ''
 
